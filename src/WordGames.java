1bacb7e1816959826884e9d556001c05bdbcf087
Sara <saracoppola996@gmail.com>
2023-08-01 19:46:15 +0200

    first commit

---
+++ src/WordGames.java
@@ -0,0 +1,20 @@
+
+public class WordGames {
+
+    /**
+     * @param word
+     * @return the string "Hello" + the word input by the user
+     */
+    public String addHelloWord(String word){
+        return "Hello " + word;
+    }
+
+    /**
+     * @param name
+     * @param surname
+     * @return concatenation of name and surname
+     */
+    public String getFullName(String name, String surname){
+        return name + " " + surname;
+    }
+}
